ab6b5c621afa3c9618f9a9cc478429ac76b61463
naingaunglinn <maythu@maymyanmarlin.com>
2022-05-19 16:06:48 +0630

    Finalize finished

--- csv_importer.py
+++ csv_importer.py
@@ -14,7 +14,8 @@ with open(filePath, 'r') as f:
     longItem = 0
     xData = []
     yData = []
-    
+    dataArray.append("airport,city,lat,long")
+    dataDiagram.append("airport,city,lat,long")
     # Retrive the CSV data
     for line in f:
         
@@ -42,49 +43,54 @@ with open(filePath, 'r') as f:
             if words[i] == 'airport':
                 airportItem = i
                 # break
-            if words[airportItem] != 'iata' and words[airportItem] != 'airport':
+            if words[airportItem] != 'iata':
                 colData_1 = words[airportItem].replace('|', ',')
+                # print (i, colData_1)
         
         # Retrive the required column city        
         for i in range(len(words)):
             if words[i] == 'city':
                 cityItem = i
                 # break
-            if words[cityItem] != 'iata' and words[cityItem] != 'city':
+            if words[cityItem] != 'iata':
                 colData_2 = words[cityItem].replace('|', ',')
+                # print (i, colData_2)
         
         # Retrive the required column latitude        
         for i in range(len(words)):
             if words[i] == 'lat':
                 latItem = i
                 # break
-            if words[latItem] != 'iata' and words[latItem] != 'lat':
+            if words[latItem] != 'iata':
                 colData_3 = words[latItem]
-                #print (words[latItem])
+                # print (i, colData_3)
         
         # Retrive the required column longtude
         for i in range(len(words)):
-            if words[latItem] != 'iata' and words[i] == 'long\n':
+            if words[i] == 'long\n':
                 longItem = i
                 break
             colData_4 = words[longItem]
-            colData_4 = colData_4.replace('\n','')
-            dataArray.append((colData_1,colData_2,colData_3,colData_4)) #insert the required data to array
+            if colData_4 != 'iata':
+                colData_4 = colData_4.replace('\n','')
+                textData = colData_1 + ',' + colData_2 + ',' + colData_3 + ',' + colData_4
+                dataArray.append(textData) #insert the required data to array
             
-            if(colData_3 != "" and colData_4 != ""):
-                latFloat = float(colData_3) 
-                longFloat = float(colData_4)
-                
-                #coordinate data validation 
-                if((latFloat >= 32 and latFloat <= 37) and (longFloat >= -100 and longFloat <= -80) ):
-                    xData.append(latFloat)
-                    yData.append(longFloat)
-                    dataDiagram.append((colData_1,colData_2,colData_3,colData_4))
-                    break
+                if(colData_3 != "" and colData_4 != ""):
+                    latFloat = float(colData_3) 
+                    longFloat = float(colData_4)
+                    
+                    #coordinate data validation 
+                    if((latFloat >= 32 and latFloat <= 37) and (longFloat >= -100 and longFloat <= -80) ):
+                        xData.append(latFloat)
+                        yData.append(longFloat)
+                        textDiagram = colData_1 + ',' + colData_2 + ',' + colData_3 + ',' + colData_4
+                        dataDiagram.append(textDiagram)
+                        break
     #Retrive the array data to text loop
     newData = list(dict.fromkeys(dataArray))  
     newDiagram = list(dict.fromkeys(dataDiagram))
-
+    
 #All row file create     
 with open('airport-data.txt', 'x') as w:
     w.write('\n'.join(str(item) for item in newData) + '\n')
